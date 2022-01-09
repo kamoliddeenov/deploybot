c34829dabb9cc78ee20c0b78956c4f46cfc59a52
Muhammad Ali <info@muhammad-ali.uz>
2022-01-09 14:57:32 +0500

    source

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -6,4 +6,4 @@ from loader import dp
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Salom, {message.from_user.full_name}!")
+    await message.answer(f"Assalomu alaykum {message.from_user.get_mention()}")
