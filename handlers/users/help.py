97293f4b10a613b4ff0f376e8ffa0dbdf5a92050
Muhammad Ali <info@muhammad-ali.uz>
2022-01-09 17:09:08 +0500

    source

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -1,13 +1,16 @@
 from aiogram import types
-from aiogram.dispatcher.filters.builtin import CommandHelp
 
 from loader import dp
 
 
-@dp.message_handler(CommandHelp())
+@dp.message_handler(commands='help')
 async def bot_help(message: types.Message):
-    text = ("Buyruqlar: ",
-            "/start - Botni ishga tushirish",
-            "/help - Yordam")
-    
-    await message.answer("\n".join(text))
+    await message.answer("""🔥 Buyruqlar
+Shunchaki menga qo'shiqchi yoki qo'shiq nomini jo'nating va men siz uchun musiqa topib beraman!
+/song - qo'shiq nomi orqali qidirish
+/artist - Qo'shiqchi ismi orqali qidirish
+/my - sizning pleylistlaringiz ro'yhati
+/top - mashhur qo'shiqlar
+/settings - sozlamalarni o'zgartirish
+
+Bog'lanish uchun aloqa: @kassir""")
